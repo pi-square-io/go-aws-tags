8b3aabef7e937a505cbdf31f72ab8fde9ef1ff1d
sabri-somrani <sabri.somrani@pi-square.io>
2022-06-29 11:39:32 +0100

    [test] test maps type

---
+++ test.go
@@ -0,0 +1,29 @@
+// _Maps_ are Go's built-in [associative data type](https://en.wikipedia.org/wiki/Associative_array)
+// (sometimes called _hashes_ or _dicts_ in other languages).
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// To create an empty map, use the builtin `make`:
+	// `make(map[key-type]val-type)`.
+	m := make(map[string]int)
+
+	// Set key/value pairs using typical `name[key] = val`
+	// syntax.
+	m["k1"] = 7
+	m["k2"] = 13
+
+	// Printing a map with e.g. `fmt.Println` will show all of
+	// its key/value pairs.
+	fmt.Println("map:", m)
+
+	// Get a value for a key with `name[key]`.
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
+
+	// The builtin `len` returns the number of key/value
+	// pairs when called on a map.
+	fmt.Println("len:", len(m))
