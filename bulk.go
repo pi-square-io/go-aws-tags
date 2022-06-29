fe13cbc5b62363d9c47955f133afa3f45ffd5590
sabri-somrani <sabri.somrani@pi-square.io>
2022-06-29 11:38:18 +0100

    [test] test maps type

--- bulk.go
+++ bulk.go
@@ -3,63 +3,78 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"log"
+	"os"
+	"pi-square-io/go-aws-tags/functions"
 )
 
+type empData struct {
+	resource string
+	id       string
+	tagkey   string
+	tagvalue string
+}
+
+
+
+
 var (
-	region    = flag.String("region", "us-east-1", "your AWS region")
-	resource  = flag.String("resource", "ec2", "your resource")
-	id        = flag.String("id", "id", "id,arn or name of resource")
-	tagkey1   = flag.String("tagkey1", "tagkey1", "key of tag resource")
-	tagvalue1 = flag.String("tagvalue1", "tagvalue1", "value of tag resource")
-	tagkey2   = flag.String("tagkey2", "tagkey2", "key of tag resource")
-	tagvalue2 = flag.String("tagvalue2", "tagvalue2", "value of tag resource")
-	tagkey3   = flag.String("tagkey3", "tagkey3", "key of tag resource")
-	tagvalue3 = flag.String("tagvalue3", "tagvalue3", "value of tag resource")
+	region   = flag.String("region", "us-east-1", "your AWS region")
+// 	resource = flag.String("resource", "resource-type", "your AWS resource type")
+// 	id       = flag.String("id", "resource-id", "your AWS resource id")
+// 	tagkey   = flag.String("tagkey", "tagkey", "tagkey")
+// 	tagvalue = flag.String("tagvalue", "tagvalue", "tagvalue")
 )
 
 func main() {
 
-	flag.Parse()
+    var mymap := map[string]string {
 
-	if *resource == "ec2" {
-		var instanceId = []string{*id}
-		TagEc2(instanceId, *tagkey1, *tagvalue1, *tagkey2, *tagvalue2, *tagkey3, *tagvalue3, *region)
+       Name
+    }
+
+	csvFile, err := os.Open("data.csv")
+	if err != nil {
+		fmt.Println(err)
 	}
-}
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
 
-func TagEc2(id []string, tagkey1 string, tagvalue1 string, tagkey2 string, tagvalue2 string, tagkey3 string, tagvalue3 string,region string) {
-
-	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String(region)},
-	)
-	svc := ec2.New(sess)
-
-	// Add tags to the created instance
-	_, errtag := svc.CreateTags(&ec2.CreateTagsInput{
-		Resources: aws.StringSlice(id),
-		Tags: []*ec2.Tag{
-			{
-				Key:   aws.String(tagkey1),
-				Value: aws.String(tagvalue1),
-			},
-			{
-				Key:   aws.String(tagkey2),
-				Value: aws.String(tagvalue2),
-			},
-			{
-				Key:   aws.String(tagkey3),
-				Value: aws.String(tagvalue3),
-			},
-		},
-	})
-	if errtag != nil {
-		log.Println("Could not create tags for instance", errtag)
-		return
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		fmt.Println(err)
 	}
+	for _, line := range csvLines {
+		emp := empData{
+			resource: line[0],
+			id:       line[1],
+			tagkey:   line[2],
+			tagvalue: line[3],
+		}
 
-	fmt.Println("Successfully tagged instance")
+		if emp.resource == "ec2" {
+			var instanceId = []string{emp.id}
+			functions.TagEc2(instanceId, emp.tagkey, emp.tagvalue, *region)
+		}
+
+		if emp.resource == "rds" {
+			functions.TagRds(emp.id, emp.tagkey, emp.tagvalue, *region)
+		}
+
+		if emp.resource == "s3" {
+			functions.TagS3(emp.id, emp.tagkey, emp.tagvalue, *region)
+		}
+
+		if emp.resource == "alb" {
+			var albArns = []string{emp.id}
+			functions.TagElb(albArns, emp.tagkey, emp.tagvalue, *region)
+		}
+
+		if emp.resource == "lambda" {
+			functions.TagLambda(emp.id, emp.tagkey, emp.tagvalue, *region)
+		}
+
+		if emp.resource == "efs" {
+			functions.TagEfs(emp.id, emp.tagkey, emp.tagvalue, *region)
+		}
+	}
 }
