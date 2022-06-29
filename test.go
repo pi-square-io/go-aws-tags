e0b0567de4f1df6c5563c2f9b6692dc77dad1808
sabri-somrani <sabri.somrani@pi-square.io>
2022-06-29 15:16:25 +0100

    [build] bulk tag resources

--- test.go
+++ test.go
@@ -1,29 +1,65 @@
-// _Maps_ are Go's built-in [associative data type](https://en.wikipedia.org/wiki/Associative_array)
-// (sometimes called _hashes_ or _dicts_ in other languages).
-
 package main
 
-import "fmt"
+import "flag"
+// import "fmt"
+import "pi-square-io/go-aws-tags/functions"
+
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+    return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+    *i = append(*i, value)
+    return nil
+}
+
+var resources arrayFlags
+var ids arrayFlags
+var tagKey arrayFlags
+var tagValue arrayFlags
+var region   = flag.String("region", "us-east-1", "your AWS region")
 
 func main() {
+    flag.Var(&resources, "resources", "Some description for this param.")
+    flag.Var(&ids, "ids", "Some description for this param.")
+    flag.Var(&tagKey, "tagKey", "Some description for this param.")
+    flag.Var(&tagValue, "tagValue", "Some description for this param.")
+    flag.Parse()
+//     fmt.Printf("Resrouces %s\n", resources)
+//     fmt.Printf("ids %s\n", ids)
+//     fmt.Printf("tagKey %s\n", tagKey)
+//     fmt.Printf("tagValue %s\n", tagValue)
+//     fmt.Printf("size %d\n", len(resources))
+//     fmt.Printf("first resource %s\n",resources[0])
 
-	// To create an empty map, use the builtin `make`:
-	// `make(map[key-type]val-type)`.
-	m := make(map[string]int)
+    for i:=0;i<len(resources);i++ {
+        if resources[i] == "ec2" {
+            var instanceId = []string{ids[i]}
+		    functions.TagEc2(instanceId, tagKey[i], tagValue[i], *region)
+         }
 
-	// Set key/value pairs using typical `name[key] = val`
-	// syntax.
-	m["k1"] = 7
-	m["k2"] = 13
+          if resources[i] == "rds" {
+		    functions.TagRds(ids[i], tagKey[i], tagValue[i], *region)
+         }
 
-	// Printing a map with e.g. `fmt.Println` will show all of
-	// its key/value pairs.
-	fmt.Println("map:", m)
+          if resources[i] == "s3" {
+		    functions.TagS3(ids[i], tagKey[i], tagValue[i], *region)
+         }
 
-	// Get a value for a key with `name[key]`.
-	v1 := m["k1"]
-	fmt.Println("v1: ", v1)
+          if resources[i] == "lambda" {
+		    functions.TagLambda(ids[i], tagKey[i], tagValue[i], *region)
+         }
+          if resources[i] == "efs" {
+		    functions.TagEfs(ids[i], tagKey[i], tagValue[i], *region)
+         }
+          if resources[i] == "rds" {
+		    functions.TagRds(ids[i], tagKey[i], tagValue[i], *region)
+         }
+        }
+        
+    }
 
-	// The builtin `len` returns the number of key/value
-	// pairs when called on a map.
-	fmt.Println("len:", len(m))
+
+
